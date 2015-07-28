5ef63f5323b484c80f78d003a735ccae4fdfb05a
Deans <deansx@gmail.com>
2015-07-27 17:43:03 -0700

    Another attempt to get it to work with pip

--- setup.py
+++ setup.py
@@ -1,40 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # coding=utf8
 
 import os
 from distutils.core import setup
 import sys
 
+VSTR = "0.04"
+DNLD_URL = "https://github.com/GripQA/grip-attendance/tarball/{0}".format(VSTR)
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-    name = 'grip-attendance',
-    py_modules = ['grip-attendance'],
-    version = '0.01',
-    description = 'Program to generate an attendance report for an event',
-    long_description=read('README.rst'),
-    author = 'Dean Stevens, GripQA',
-    author_email = 'support@grip.qa',
-    url = 'https://github.com/GripQA/grip-attendance', # URL to the github repo
-    download_url = 'https://github.com/GripQA/grip-attendance/tarball/0.01',
-    keywords = ['attendance',
-                'webinar',
-                'meetings',
-                'registration',
-                'attendee'
+    name = "grip-attendance",
+    scripts = ["grip-attendance.py"],
+    version = VSTR,
+    description = "Program to generate an attendance report for an event",
+    long_description=read("README.rst"),
+    author = "Dean Stevens, GripQA",
+    author_email = "support@grip.qa",
+    url = "https://github.com/GripQA/grip-attendance", # URL to the github repo
+    download_url = DNLD_URL,
+    keywords = ["attendance",
+                "webinar",
+                "meetings",
+                "registration",
+                "attendee"
     ],
-    classifiers = ['Programming Language :: Python',
-                   'Programming Language :: Python :: 3.4',
-                   'Operating System :: OS Independent',
-                   'Development Status :: 3 - Alpha',
-                   'Intended Audience :: End Users/Desktop',
-                   'License :: OSI Approved :: Apache Software License',
-                   'Natural Language :: English',
-                   'Topic :: Office/Business',
-                   'Topic :: Utilities',
+    classifiers = ["Programming Language :: Python",
+                   "Programming Language :: Python :: 3.4",
+                   "Operating System :: OS Independent",
+                   "Development Status :: 3 - Alpha",
+                   "Intended Audience :: End Users/Desktop",
+                   "License :: OSI Approved :: Apache Software License",
+                   "Natural Language :: English",
+                   "Topic :: Office/Business",
+                   "Topic :: Utilities",
                    
     ],
 )
